aa55ec0c1bc6d9fe120eccce4be41d28172f677f
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-01-11 14:12:42 -0800

    header and feedback listing components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className='container'>
+        <FeedbackList />
+      </div>
+    </>
+  );
+}
+
+export default App;
