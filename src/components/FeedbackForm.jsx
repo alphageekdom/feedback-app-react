fcaaead0ff36ca3791b446c269f4d6d0e5f57f9d
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-01-11 15:09:17 -0800

    Form component and button component

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import Card from './shared/Card';
+import { useState } from 'react';
+import Button from './shared/Button';
+
+const FeedbackForm = () => {
+  const [text, setText] = useState('');
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <Card>
+      <form action=''>
+        <h2>How would you rate your service with us?</h2>
+        <div className='input-group'>
+          <input
+            onChange={handleTextChange}
+            type='text'
+            placeholder='Write a review'
+            value={text}
+          />
+          <Button type='submit'>Send</Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default FeedbackForm;
