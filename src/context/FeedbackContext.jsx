11dfed9138d35ad13c1a8864f975bcfac9ada53f
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-01-11 20:03:58 -0800

    edit feedback

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -7,24 +7,57 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'The item is from context',
+      text: 'The item is from context1',
       rating: 10,
+    },
+    {
+      id: 2,
+      text: 'The item is from context2',
+      rating: 9,
+    },
+    {
+      id: 3,
+      text: 'The item is from context3',
+      rating: 8,
     },
   ]);
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
+  //   Add Feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
   };
 
+  //   Delete Feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
+  //   Edit Feedback
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
   return (
-    <FeedbackContext.Provider value={{ feedback, deleteFeedback, addFeedback }}>
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        feedbackEdit,
+      }}
+    >
       {children}
     </FeedbackContext.Provider>
   );
